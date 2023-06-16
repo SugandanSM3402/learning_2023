4ccf68b695f3ba7a837dfcda8bf332e1cbdfad14
sugandan <sugandansm@gmail.com>
2023-06-16 12:26:10 +0530

    upadated

---
+++ Module_1/Day3/Level1/Problem5/Problem5.c
@@ -0,0 +1,2 @@
+/*Write a program to find both smallest and largest digits in given n numbers
+For example let's say we have n = 3, and n1 = 8, n2 = 156, n3 = 123450, the result will be "Not Valid", 1 and 6, 0 and 5*/
